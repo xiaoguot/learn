e642e42e85da2c11e97934fe109c4d76b9eccb26
guot <97510914@qq.com>
2022-04-16 23:29:19 +0800

    ts+axios封装

--- Vue3/vue3-ts-cms/src/App.vue
+++ Vue3/vue3-ts-cms/src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <h2>{{ $store.state.name }}</h2>
 
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="success">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+    <el-button type="warning">警告按钮</el-button>
+    <el-button type="danger">危险按钮</el-button>
+
     <router-link to="/login">登录</router-link>
     <router-link to="/main">首页</router-link>
     <router-view></router-view>
--- Vue3/vue3-ts-cms/src/main.ts
+++ Vue3/vue3-ts-cms/src/main.ts
@@ -1,6 +1,54 @@
 import { createApp } from 'vue'
+import { globalRegister } from './global'
+
 import App from './App.vue'
+
+//import './service/axios_demo'
+import hyRequest from './service'
 import router from './router'
 import store from './store'
 
-createApp(App).use(router).use(store).mount('#app')
+const app = createApp(App)
+
+// 注册element-plus/其他
+app.use(globalRegister)
+app.use(router)
+app.use(store)
+
+app.mount('#app')
+
+console.log(process.env.VUE_APP_BASE_URL)
+console.log(process.env.VUE_APP_BASE_NAME)
+
+// hyRequest.request({
+//   url: '/home/multidata',
+//   method: 'GET',
+//   headers: {},
+//   interceptors: {
+//     requestInterceptor: (config) => {
+//       console.log('单独请求的config')
+//       return config
+//     },
+//     responseInterceptor: (res) => {
+//       console.log('单独相应的response')
+//       return res
+//     }
+//   }
+// })
+
+interface DataType {
+  data: any
+  returnCode: string
+  success: boolean
+}
+
+hyRequest
+  .get<DataType>({
+    url: '/home/multidata',
+    showLoading: false
+  })
+  .then((res) => {
+    console.log(res.data)
+    console.log(res.returnCode)
+    console.log(res.success)
+  })
